436e4e290886d194c04afc7cb4bc195c4589701e
Anna Futryk <121104832+AnnaFutryk@users.noreply.github.com>
2023-09-02 13:50:00 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { useFonts } from "expo-font";
+import { StyleSheet, View } from "react-native";
+import { LoginScreen } from "./src/Screens/LoginScreen/LoginScreen";
+import { PostsScreen } from "./src/Screens/PostsScreen/PostsScreen";
+import { RegistrationScreen } from "./src/Screens/RegistrationScreen/RegistrationScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+      {/* <PostsScreen /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+});
